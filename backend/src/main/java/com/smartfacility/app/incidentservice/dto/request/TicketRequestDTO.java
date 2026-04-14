5adaab8fd7cc18642e238083195ef49afac6d20e
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2026-04-14 20:45:16 +0530

    create TicketRequestDTO.java /
    DTO → how data is sent/received through the API.
    ---
     The DTO is a separate class that only contains the fields you want to receive or send. Entity = database shape. DTO = API shape. Always keep them separate.

---
+++ backend/src/main/java/com/smartfacility/app/incidentservice/dto/request/TicketRequestDTO.java
@@ -0,0 +1,33 @@
+package com.smartfacility.app.incidentservice.dto.request;
+
+import com.smartfacility.app.incidentservice.enums.Category;
+import com.smartfacility.app.incidentservice.enums.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TicketRequestDTO {
+
+    @NotBlank(message = "Resource location is requireed")
+    private String resourceLocation;
+
+    @NotNull(message = "category is required")
+    private Category category;
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 10, max = 1000, message = "Description must be between 10 and 1000 characters")
+    private String description;
+
+    @NotNull(message = "Priority is required")
+    private Priority priority;
+
+    @NotBlank(message = "Contact details are required")
+    private String contactDetails;
+
+
+}
