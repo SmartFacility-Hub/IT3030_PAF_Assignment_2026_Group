b38abc1472fee4630cc6665abd2583017459ea65
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2026-04-14 20:50:56 +0530

    Add CommentRequestDTO with validation

---
+++ backend/src/main/java/com/smartfacility/app/incidentservice/dto/request/CommentRequestDTO.java
@@ -0,0 +1,10 @@
+package com.smartfacility.app.incidentservice.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CommentRequestDTO {
+    @NotBlank(message =  "Comment content cannot be empty")
+    @Size(max = 1000, message =  "Comment cannot exceed 1000 characters")
+    private String content;
+}
