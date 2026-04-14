d08bc3432b0fabda70e3a786bfd4850f57dfa41a
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2026-04-14 21:59:15 +0530

    Add TicketResponseDTO

---
+++ backend/src/main/java/com/smartfacility/app/incidentservice/dto/response/TicketResponseDTO.java
@@ -0,0 +1,26 @@
+package com.smartfacility.app.incidentservice.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.smartfacility.app.incidentservice.enums.Category;
+import com.smartfacility.app.incidentservice.enums.Priority;
+import com.smartfacility.app.incidentservice.enums.TicketStatus;
+
+public class TicketResponseDTO {
+    private Long id;
+    private String resourceLocation;
+    private Category category;
+    private String description;
+    private Priority priority;
+    private TicketStatus status;
+    private String contactDetails;
+    private String createdBy;
+    private String assignedTo;
+    private String resolutionNotes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<AttachmentResponseDTO> attachments;
+    private List<CommentResponseDTO> comments;
+    
+}
