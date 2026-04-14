f3366d9767cb8c9b0a21dd0ace3d66268656d81b
Dinura Sanmith <153044444+Sanmith2002@users.noreply.github.com>
2026-04-14 16:23:08 +0530

    add lecturedashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AdminDashboard from './pages/AdminDashboard';
 import StudentDashboard from './pages/Studentdashboard';
+import LectureDashboard from './pages/Lecturerdashboard';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/lecturer" element={<LectureDashboard />} />
       </Routes>
     </Router>
   );
