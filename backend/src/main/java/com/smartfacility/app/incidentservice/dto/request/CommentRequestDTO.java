a97802af2b6290642576f7a99c1a802c37218075
ChanakaWickramasingha <amanthachanaka223@gmail.com>
2026-04-16 16:14:26 +0530

    update CommentRequestDTO.java
    add getters and setters

--- backend/src/main/java/com/smartfacility/app/incidentservice/dto/request/CommentRequestDTO.java
+++ backend/src/main/java/com/smartfacility/app/incidentservice/dto/request/CommentRequestDTO.java
@@ -2,7 +2,11 @@ package com.smartfacility.app.incidentservice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CommentRequestDTO {
     @NotBlank(message =  "Comment content cannot be empty")
     @Size(max = 1000, message =  "Comment cannot exceed 1000 characters")
